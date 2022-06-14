6d9e345a681971ca9a51a69df9595985c6127431
MykytiukSS <m.sergeysergeyevich@gmail.com>
2022-06-14 17:30:28 +0300

    added scanner in main

--- src/bigBangTheory/Main.java
+++ src/bigBangTheory/Main.java
@@ -1,11 +1,16 @@
 package bigBangTheory;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		BingBangDeque joke = new BingBangDeque();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number");
+		int count = sc.nextInt();
 		
-		joke.bangDequeue(2);
+		joke.bangDequeue(count);
 
 	}
 
